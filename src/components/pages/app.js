37ee811fe00944215c8e1697b921af278b683e62
mspsohan <mspsohan@gmail.com>
2024-02-11 23:57:33 +0600

    Added Two Section

---
+++ src/components/pages/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+   return (
+      <div>
+         Mobile App
+      </div>
+   );
+};
+
+export default App;
---
+++ src/components/pages/teams.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Teams = () => {
+   return (
+      <div>
+         Our Teams
+      </div>
+   );
+};
+
+export default Teams;
